1dbb1d6f0b27d803372b7d03c31ffa73c0dd3c0f
Thomas <thomastimmer11@hotmail.com>
2023-10-10 22:36:58 +0200

    feat: clean up types

--- index.test.ts
+++ index.test.ts
@@ -1,6 +1,15 @@
 import { test, expect } from "bun:test";
 import { Result } from "./index";
 
+const functionThatThrows = () => {
+  try {
+    (42).toString(0);
+    return Result.ok(1);
+  } catch (error) {
+    return Result.error(error);
+  }
+};
+
 test("new unwrap", () => {
   const r = new Result(null, null, true);
   expect(r.unwrap()).toBe(null);
@@ -16,6 +25,13 @@ test("error unwrap", () => {
   const r = Result.error("Broke");
   expect(() => r.unwrap()).toThrow("Expected ok, but found error 'Broke'");
   expect(r.unwrapError()).toBe("Broke");
+});
+
+test("error javascript error unwrap", () => {
+  const r = functionThatThrows();
+  expect(() => r.unwrap()).toThrow(
+    "toString() radix argument must be between 2 and 36",
+  );
 });
 
 test("ok map", () => {
@@ -42,16 +58,28 @@ test("error mapError", () => {
   expect(updatedR.unwrapError()).toBe("1");
 });
 
+test("ok toArray", () => {
+  const r = Result.ok(1);
+  const updatedR = r.map((x) => `${x}`);
+  expect(updatedR.toArray()).toStrictEqual(["1", null]);
+});
+
+test("error toArray", () => {
+  const r = Result.error(1);
+  const updatedR = r.mapError((x) => `${x}`);
+  expect(updatedR.toArray()).toStrictEqual([null, "1"]);
+});
+
 test("chain works with types", () => {
   const out = Result.ok(1)
     .map((x) => x + 1)
     .map((x) => 2 * x)
-    .flatMap((x) => Result.error(`${x}`))
+    .flatMap((x) => (1 + 2 ? Result.error(`${x}`) : Result.ok(1)))
     .map((x) => x + 3)
-    .mapError((x) => x.concat("2") )
+    .mapError((x) => x.concat("2"))
     .flatMapError((x) => Result.ok(Number(x)))
     .map((x) => x + 1)
-    .unwrap()
+    .unwrap();
 
-    expect(out).toBe(43)
+  expect(out).toBe(43);
 });
--- index.ts
+++ index.ts
@@ -1,3 +1,7 @@
+function isError(err: unknown): err is Error {
+  return err instanceof Error;
+}
+
 export class ResultError extends Error {
   constructor(message: string) {
     super(message);
@@ -8,72 +12,111 @@ export class ResultError extends Error {
 export class Result<T, E> {
   #value: T;
   #error: E;
-  ok: boolean;
+  /**
+   * Returns true when Result holds an ok value
+   */
+  isOk: boolean;
   constructor(value: T, error: E, isOk: boolean) {
-    this.ok = isOk;
+    this.isOk = isOk;
     this.#error = error;
     this.#value = value;
   }
 
+  /**
+   * Create Result with an ok value
+   */
   static ok<T, E>(value: T): Result<T, never> {
     return new Result(value, null, true) as Result<T, never>;
   }
 
+  /**
+   * Create Result with an error value
+   */
   static error<T, E>(error: E): Result<never, E> {
     return new Result(null, error, false) as Result<never, E>;
   }
 
+  /**
+   * Returns true when Result holds an error value
+   */
+  get isErr(): boolean {
+    return !this.isOk;
+  }
+
+  /**
+   * Returns the ok and error parts in an array
+   */
   toArray(): [T, E] {
     return [this.#value, this.#error];
   }
 
-  map<R>(func: (value: T) => R): Result<R, E> | Result<T, E> {
-    if (this.ok) {
+  map<R>(func: (value: T) => R): Result<R, E> {
+    if (this.isOk) {
       return new Result(func(this.#value), this.#error, true);
     } else {
-      return this;
+      return this as unknown as Result<R, E>;
     }
   }
 
-  mapError<R>(func: (value: E) => R): Result<T, E> | Result<T, R> {
-    if (!this.ok) {
+  mapError<R>(func: (value: E) => R): Result<T, R> {
+    if (this.isErr) {
       return new Result(this.#value, func(this.#error), false);
     } else {
-      return this;
+      return this as unknown as Result<T, R>;
     }
   }
 
   // is this a nice name?
-  flatMap<R, F>(func: (value: T) => Result<R, F>): Result<R, F> | Result<T, E> {
-    if (this.ok) {
+  flatMap<R, F>(func: (value: T) => Result<R, F>): Result<R, E | F> {
+    if (this.isOk) {
       return func(this.#value);
     } else {
-      return this;
+      return this as unknown as Result<never, E>;
     }
   }
 
   // is this a nice name?
-  flatMapError<R, F>(func: (value: E) => Result<R, F>): Result<R, F> | Result<T, E> {
-    if (!this.ok) {
+  flatMapError<R, F>(func: (value: E) => Result<R, F>): Result<T | R, F> {
+    if (this.isErr) {
       return func(this.#error);
     } else {
-      return this;
+      return this as unknown as Result<T, never>;
     }
   }
 
   unwrap() {
-    if (this.ok) {
+    if (this.isOk) {
       return this.#value;
     } else {
-      throw new ResultError(`Expected ok, but found error '${this.#error}'`);
+      if (isError(this.#error)) {
+        throw this.#error;
+      } else {
+        throw new ResultError(`Expected ok, but found error '${this.#error}'`);
+      }
     }
   }
 
   unwrapError() {
-    if (!this.ok) {
+    if (this.isErr) {
       return this.#error;
     } else {
       throw new ResultError(`Expected error, but found ok '${this.#value}'`);
+    }
+  }
+
+  unpack(): T | null {
+    if (this.isOk) {
+      return this.#value;
+    } else {
+      return null;
+    }
+  }
+
+  unpackError(): E | null {
+    if (this.isErr) {
+      return this.#error;
+    } else {
+      return null;
     }
   }
 }
